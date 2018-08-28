52a09de1ebe487992e44f37e95f7921888a597e1
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-28 11:50:18 -0700

    prepping for create react app

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,14 +1,26 @@
 import React from "react";
 import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 import Splash from './splash/splash';
+// import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
+// import { red, green, purple } from 'material-ui/colors'
+
+// const theme = createMuiTheme({
+//   palette: {
+//     primary: purple,
+//     secondary: green,
+//     error: red,
+//   },
+// });
 
 const App = () => (
-    <div>
-  
-      <Switch>
+  <div>
+
+    <Switch>
+      {/* <MuiThemeProvider theme={theme}> */}
         <Route exact path="/" component={Splash} />
-      </Switch>
-    </div>
-  );
-  
-  export default App;
+      {/* </MuiThemeProvider> */}
+    </Switch>
+  </div>
+);
+
+export default App;
--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -5,12 +5,19 @@ const splash = () => {
     return (
         <div className="splash">
             <img className="logo-img" src={'./images/clockpng.png'} />
+            <br/>
             <h1>Watchie</h1>
+            <br/>
             <Button variant="outlined" color="secondary" >
                 Sign Up
             </Button>
+            <br/>
             <Button variant="outlined" color="secondary" >
                 Log In
+            </Button>
+            <br/>
+            <Button variant="outlined" color="secondary" >
+                Demo User
             </Button>
         </div>
     );
