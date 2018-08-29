f7e37a817f31a07ff2d7c43de49fae2f024ff171
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-29 12:32:30 -0700

    add history container

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,15 +3,19 @@ import { Route, Redirect, Switch, Link, HashRouter } from "react-router-dom";
 import Splash from './splash/splash';
 import DashboardContainer from './dashboard/dashboard_container';
 import MapContainer from './map/map_container.js';
+import HistoryContainer from './history/history_container.js';
+import LocationContainer from './location/location_container.js';
 
 const App = () => (
   <div>
 
     <Switch>
       {/* <MuiThemeProvider theme={theme}> */}
-        <Route exact path="/" component={Splash} />
-        <Route exact path="/dashboard" component={DashboardContainer} />
-        <Route exact path="/map" component={MapContainer} />
+      <Route exact path="/" component={Splash} />
+      <Route exact path="/dashboard" component={DashboardContainer} />
+      <Route exact path="/map" component={MapContainer} />
+      <Route exact path="/history" component={HistoryContainer} />
+      <Route exact path="/location" component={LocationContainer} />
       {/* </MuiThemeProvider> */}
     </Switch>
   </div>
