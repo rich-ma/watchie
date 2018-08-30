375193ee1e63014a7618b0a6e4a3923e4c88b39f
nigelrodrigues15 <nigel1@ualberta.ca>
2018-08-30 10:38:17 -0700

    update history

--- frontend/src/components/history/history.jsx
+++ frontend/src/components/history/history.jsx
@@ -5,6 +5,19 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import Divider from '@material-ui/core/Divider';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+import Button from '@material-ui/core/Button';
+
 class History extends React.Component {
     constructor(props) {
         super(props);
@@ -22,37 +35,25 @@ class History extends React.Component {
                 <h1>This is the history page</h1>
                 <br />
                 <div className="history-list">
-                <List component="nav">
-                    <ListItem button>
-                        <ListItemText primary="History Item 1" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button divider>
-                        <ListItemText primary="History Item 2" />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemText primary="History Item 3" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button>
-                        <ListItemText primary="History Item 4" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button>
-                        <ListItemText primary="History Item 1" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button divider>
-                        <ListItemText primary="History Item 2" />
-                    </ListItem>
-                    <ListItem button>
-                        <ListItemText primary="History Item 3" />
-                    </ListItem>
-                    <Divider />
-                    <ListItem button>
-                        <ListItemText primary="History Item 4" />
-                    </ListItem>
-                </List>
+                    <List component="nav">
+                        <Divider />
+                        <ListItem button>
+                            <ListItemText primary="History Item 1" />
+                        </ListItem>
+                        <Divider />
+                        <ListItem button divider>
+                            <ListItemText primary="History Item 2" />
+                        </ListItem>
+                        <ListItem button>
+                            <ListItemText primary="History Item 3" />
+                        </ListItem>
+                        <Divider />
+                        <ListItem button>
+                            <ListItemText primary="History Item 4" />
+                        </ListItem>
+                        <Divider />
+                    </List>
+
                 </div>
             </div>
         );
