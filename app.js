755b093e69b85a6d54665222d7bd8b17b314d835
Steven Lum <stevielum1@gmail.com>
2018-09-03 12:24:28 -0700

    Update static assets path

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.get("/", (request, res) => {
 });
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('frontend/build'));
+  app.use(express.static('frontend'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
   });
 } else {
   app.use(express.static('frontend'));
