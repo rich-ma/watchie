fe6ccc8703063c181d69439a41872c56c55ccc6b
Steven Lum <stevielum1@gmail.com>
2018-09-03 08:50:40 -0700

    Update files for heroku deployment

--- app.js
+++ app.js
@@ -23,6 +23,13 @@ app.get("/", (request, res) => {
   res.sendFile(path.join(__dirname, "./frontend/index.html"));
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.use(bodyParser.urlencoded({
   extended: false
 }));
